14c0b38924f9b5c710964d49ba5824fecf20c2cd
smilekaka <jinchunliang1988@163.com>
2020-06-08 16:24:53 +0800

    添加获取token的方法

--- src/main/java/com/hello/springboot/hellospringboot/config/GlobalCorsConfig.java
+++ src/main/java/com/hello/springboot/hellospringboot/config/GlobalCorsConfig.java
@@ -1,30 +1,46 @@
 package com.hello.springboot.hellospringboot.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class GlobalCorsConfig {
+public class GlobalCorsConfig implements WebMvcConfigurer {
 
-    /**
-     * 允许跨域调用的过滤器
-     */
-    @Bean
-    public CorsFilter corsFilter() {
-        CorsConfiguration config = new CorsConfiguration();
-        //允许所有域名进行跨域调用
-        config.addAllowedOrigin("*");
-        //允许跨越发送cookie
-        config.setAllowCredentials(true);
-        //放行全部原始头信息
-        config.addAllowedHeader("*");
-        //允许所有请求方法跨域调用
-        config.addAllowedMethod("*");
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
+//    /**
+//     * 允许跨域调用的过滤器
+//     */
+//    @Bean
+//    public FilterRegistrationBean<CorsFilter> corsFilter() {
+//        CorsConfiguration config = new CorsConfiguration();
+//        // 允许所有域名进行跨域调用
+//        config.addAllowedOrigin("*");
+//        // 允许跨越发送cookie
+//        config.setAllowCredentials(true);
+//        // 放行全部原始头信息
+//        config.addAllowedHeader("*");
+//        // 允许所有请求方法跨域调用
+//        config.addAllowedMethod("*");
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", config);
+//        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<CorsFilter>(new CorsFilter(source));
+//        // 这个顺序很重要哦，为避免麻烦请设置在最前
+//        bean.setOrder(0);
+//        return bean;
+//    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(false)
+                .allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS")
+                .maxAge(3600);
     }
+
 }
--- src/main/java/com/hello/springboot/hellospringboot/config/SecurityConfig.java
+++ src/main/java/com/hello/springboot/hellospringboot/config/SecurityConfig.java
@@ -7,20 +7,29 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@EnableOAuth2Sso
-@Configuration
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//@EnableOAuth2Sso
+//@Configuration
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig /*extends WebSecurityConfigurerAdapter*/ {
 
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http.antMatcher("/**")
-                .authorizeRequests()
-                .antMatchers(HttpMethod.OPTIONS)//跨域请求会先进行一次options请求
-                .permitAll()
-                .antMatchers("/", "/login**").permitAll()
-                .anyRequest()
-                .authenticated();
-    }
+//    @Override
+//    public void configure(HttpSecurity http) throws Exception {
+//        http.cors().and()
+////                .csrf().disable()
+////		        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED).and().antMatcher("/**")
+//                .authorizeRequests()
+//                //跨域请求会先进行一次options请求
+//                .antMatchers(HttpMethod.OPTIONS).permitAll()
+//                .antMatchers("/", "/login**").permitAll()
+//                .anyRequest()
+//                .authenticated();
+//
+////        允许所有应用请求
+////        http.csrf().disable()
+//////		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED).and()
+////                .authorizeRequests().antMatchers(HttpMethod.OPTIONS).permitAll()// 跨域请求会先进行一次options请求
+////                .anyRequest().permitAll().and().logout().permitAll();
+////        http.headers().cacheControl();
+//    }
 
 }
